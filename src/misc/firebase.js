b73092390ba16777335dbef7ac91b016f5ec2352
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2023-08-20 15:53:22 -0700

    firebase init

---
+++ src/misc/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Xz0yHmK2UnXvZM63mN4AKAGEYtVZgFU",
+  authDomain: "chat-web-app-2b863.firebaseapp.com",
+  projectId: "chat-web-app-2b863",
+  storageBucket: "chat-web-app-2b863.appspot.com",
+  messagingSenderId: "596123577806",
+  appId: "1:596123577806:web:533b9ebd1acf26991ecf9f"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
