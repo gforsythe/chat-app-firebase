bbc692e87009c9e79caaf6e8ba7a0d26094269ba
Gabriel Forsythe <gabe.forsythe@gmail.com>
2023-09-20 16:01:32 -0700

    numerous updates on package json and added routes private and public

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,17 @@
+import {Redirect} from 'react-router';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+const PublicRoute = ({children, ...routeProps}) => {
+  const profile = false;
+
+  if (profile){
+    return <Redirect to="/"/>
+  }
+
+  return (
+    <Route {...routeProps}>
+      {children}
+    </Route>
+  )
+}
+
+export default PublicRoute;
