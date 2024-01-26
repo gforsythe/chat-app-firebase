7962b1911b7a864b51e2b192eb0470ab92cbfda4
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-01-25 19:09:05 -0800

    created a drawer heading, title, and a signout function

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import {useProfileHook } from '../../context/profile.context';
 
-function Dashboard() {
+function Dashboard({onSignOut}) {
+  const {profile} = useProfileHook();
   return (
-    <div>hello this is the dashboard</div>
+    <>
+    <Drawer.Header>
+      <Drawer.Title>
+        Dashboard
+      </Drawer.Title>
+    </Drawer.Header>
+    <Drawer.Body>
+      <h3>Hey {profile.name}</h3>
+    </Drawer.Body>
+    <Drawer.Footer>
+      <Button block color='red' onClick={onSignOut} >Sign Out</Button>
+    </Drawer.Footer>
+    </>
   )
 }
+
 
 export default Dashboard
