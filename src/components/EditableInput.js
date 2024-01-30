1412f050a6651bd35f2e3c09e55815eafe27af99
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-01-29 19:11:10 -0800

    added editable component and basic functionality to input button

---
+++ src/components/EditableInput.js
@@ -0,0 +1,45 @@
+import React, { useCallback, useState } from 'react';
+import { Icon, Input, InputGroup } from 'rsuite';
+
+function EditableInput({ initalValue, onSave, label = null, placeholder = "write your value", emptyMsg = "input is empty", ...inputProps }) {
+
+  const [input, setInput] = useState(initalValue);
+  const [isEditable, setEditable] = useState(false);
+
+  const onInputChange = useCallback((value) => {
+    setInput(value);
+  }, []);
+
+const onEditClick = useCallback(() => {
+  setEditable(p => !p);
+  setInput(initalValue)
+}, [initalValue])
+
+const onSaveClick = async () => {
+  const trimmed = input.trim();
+  if (trimmed === "") {
+    Alert.info(emptyMsg, 4000)
+  }
+  if (trimmed !== initalValue) {
+    await onSave(trimmed)
+  }
+  setEditable(false)
+}
+
+  return (
+    <div>
+      {label}
+      <InputGroup>
+      <Input {...inputProps} disabled={!isEditable} placeholder={placeholder} value={input} onChange={onInputChange} />
+      <InputGroup.Button onClick={onEditClick}>
+        <Icon icon={isEditable ? "close" : "edit2"}></Icon>
+      </InputGroup.Button>
+      {isEditable &&  <InputGroup.Button onClick={onSaveClick}>
+        <Icon icon={"check"}></Icon>
+      </InputGroup.Button> }
+      </InputGroup>
+    </div>
+  );
+}
+
+export default EditableInput;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import {useProfileHook } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 function Dashboard({onSignOut}) {
   const {profile} = useProfileHook();
+  const onSave = async (newData) => {
+    console.log("This is the new data:",newData);
+  }
   return (
     <>
     <Drawer.Header>
@@ -13,6 +17,8 @@ function Dashboard({onSignOut}) {
     </Drawer.Header>
     <Drawer.Body>
       <h3>Hey {profile.name}</h3>
+      <Divider/>
+      <EditableInput name={"nickname"} initalValue={profile.name} onSave={onSave} label={<h6 className='mb-2'>Nickname</h6>}/>
     </Drawer.Body>
     <Drawer.Footer>
       <Button block color='red' onClick={onSignOut} >Sign Out</Button>
