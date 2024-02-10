650818ef80fa8e7e48a7f847ccbed11839cdb2da
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-02-10 13:01:03 -0800

    added links to display the right chat ID

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,8 +1,38 @@
+import { useParams } from "react-router";
+import { useRooms } from "../../context/rooms.context";
+import { Loader } from "rsuite";
+import Top from "../../components/chat-window/top/";
+import Messages from "../../components/chat-window/messages/";
+import Bottom from "../../components/chat-window/bottom/";
+
 
 function Chat() {
+  const { chatId } = useParams();
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading ..." speed="slow" />
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId)
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page"> Chat {chatId} Not Found ...</h6>
+  }
+
   return (
-    <div>Hello!</div>
-  )
+    <>
+      <div className="chat-top">
+        <Top />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <Bottom />
+      </div>
+    </>
+  );
 }
 
-export default Chat
+export default Chat;
