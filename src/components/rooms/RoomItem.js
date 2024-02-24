f3693ac7148ea66284eed128f0a7b93b1372e483
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-02-23 18:09:01 -0800

    Last Message displays in sidebar

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,19 +1,31 @@
 import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../ProfileAvatar';
 
-function RoomItem({room}) {
-  const {createdAt, name} = room
+function RoomItem({ room }) {
+  const { createdAt, name, lastMessage } = room;
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">{name}</h3>
         <TimeAgo className='font-normal text-black-45'
-          datetime={new Date(createdAt)}
-          
+          datetime={lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)}
+
         />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        <span>No Messages yet...</span>
+        {
+          lastMessage ? <>
+            <div className='d-flex align-items-center'>
+              <ProfileAvatar src={lastMessage.author.avatar} name={lastMessage.author.name} size="sm" />
+              <div className='text-disappear ml-2'>
+                <div className='italic'>{lastMessage.author.name}</div>
+                <span>{lastMessage.text}</span>
+              </div>
+            </div>
+          </> : <span>No Messages yet...</span>
+        }
+
       </div>
     </div>
   );
