1999569e717a97a01876e6740faa5ed6d1bf409e
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-02-26 10:58:35 -0800

    diplay online presence created and rendered

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,12 +1,14 @@
 import TimeAgo from "timeago-react";
 import ProfileAvatar from "../../ProfileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
+import PresenceDot from "../../PresenceDot";
 
 const  MessageItem = ({message}) => {
   const {author, createdAt, text} = message;
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid}/>
       <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size="xs"/>
       <ProfileInfoBtnModal appearence="link" profile={author} className="p-0 ml-1 text-black"/>
       <TimeAgo className='font-normal text-black-45 ml-2'
