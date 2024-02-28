967519daea1a5feda9b66e89b3bf3c15d79dc4dd
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-02-27 23:49:14 -0800

    created the like icon and basic functionality

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,5 +1,6 @@
 import TimeAgo from "timeago-react";
 import { memo } from "react";
+import IconBtnControl from "./IconBtnControl";
 import { Button } from "rsuite";
 import ProfileAvatar from "../../ProfileAvatar";
 import ProfileInfoBtnModal from "./ProfileInfoBtnModal";
@@ -8,37 +9,37 @@ import { useHover } from "../../../misc/custom-hooks";
 import { useCurrentRoom } from "../../../context/current-room.context";
 import { auth } from "../../../misc/firebase";
 
-const  MessageItem = ({message, handleAdmin}) => {
-  const {author, createdAt, text} = message;
-  const [selfRef, isHovered] = useHover()
+const MessageItem = ({ message, handleAdmin }) => {
+  const { author, createdAt, text } = message;
+  const [selfRef, isHovered] = useHover();
   const isAdmin = useCurrentRoom(v => v.isAdmin);
   const admins = useCurrentRoom(v => v.admins);
   const isMsgAuthorAdmin = admins.includes(author.uid);
-  const isAuthor = auth.currentUser.uid === author.uid; 
+  const isAuthor = auth.currentUser.uid === author.uid;
 
 
-  const canGrantAdmin = isAdmin && !isAuthor; 
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
-    <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02':''}`} ref={selfRef}>
+    <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`} ref={selfRef}>
       <div className="d-flex align-items-center font-bolder mb-1">
-        <PresenceDot uid={author.uid}/>
-      <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size="xs"/>
+        <PresenceDot uid={author.uid} />
+        <ProfileAvatar src={author.avatar} name={author.name} className='ml-1' size="xs" />
 
-      <ProfileInfoBtnModal appearence="link" profile={author} className="p-0 ml-1 text-black">
-        {canGrantAdmin && 
-        <Button block onClick={()=>handleAdmin(author.uid)} color="blue">{isMsgAuthorAdmin ? "Remove Admin Permmision?": "Give Admin For This Room"}</Button>}
-      </ProfileInfoBtnModal>
-      <TimeAgo className='font-normal text-black-45 ml-2'
-        datetime={createdAt}
+        <ProfileInfoBtnModal appearence="link" profile={author} className="p-0 ml-1 text-black">
+          {canGrantAdmin &&
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">{isMsgAuthorAdmin ? "Remove Admin Permmision?" : "Give Admin For This Room"}</Button>}
+        </ProfileInfoBtnModal>
+        <TimeAgo className='font-normal text-black-45 ml-2'
+          datetime={createdAt}
         />
-
-      </div> 
+        <IconBtnControl {...(true ? { color: "red" } : {})} isVisible iconName="heart" tooltip="like this message" onClick={() => { }} badgeContent={5} />
+      </div>
       <div>
         <span className="word-break-all">{text}</span>
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default memo(MessageItem)
+export default memo(MessageItem);
