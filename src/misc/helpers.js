e4853aabb32560197031398effdbffff9af722b6
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-04-03 15:38:54 -0700

    service worker replaced

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -56,3 +56,14 @@ export function groupByDate(arr, groupingKeyFn) {
     return result;
   }, {});
 }
+
+
+export const iisLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
