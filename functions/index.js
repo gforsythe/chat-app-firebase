d0224e3ad77d0f366c0972c9f662f25df3c6debd
Gabriel Forsythe <gabe.forsythe@gmail.com>
2024-04-15 16:25:05 -0700

    fixed something in index.js

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const functions = require("firebase-functions")
-const admin = require("firebase-admin")
-const logger = require("firebase-functions/logger");
-
-const  {sendFcm} = require('./src/fcm')
-
-const serviceAccount = require("./service-account.json");
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://chat-web-app-2b863-default-rtdb.firebaseio.com"
+  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
 });
 
-
+const { sendFcm } = require('./src/fcm');
 
 exports.sendFcm = sendFcm;
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
